a387add38fb383a39ff36d67d2a561a542c51d17
Pau Duran Casas <32158939+DurCas@users.noreply.github.com>
2020-09-01 16:53:07 +0200

    Update funcions2.php
    No duplicar la consulta

--- Registre/php/funcions2.php
+++ Registre/php/funcions2.php
@@ -13,5 +13,5 @@
     $query  = "INSERT INTO usuari (id, nom, cognom, email, telefon) VALUES('$id', '$nom', '$cognom', '$email', '$telf')";
     echo $query;
     $con->query($query);
-    $res=$con->query($query);
+    //$res=$con->query($query);
     ?>
